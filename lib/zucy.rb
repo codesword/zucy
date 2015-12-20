dddd843bc44a68a0067ccd5b1c1011ed72f45484
Ikem Okonkwo <ikem.okonkwo@andela.com>
2015-12-20 18:06:07 +0100

    create initial application class

--- lib/zucy.rb
+++ lib/zucy.rb
@@ -1,5 +1,23 @@
 require "zucy/version"
 
 module Zucy
+  class Application
 
+    def call(env)
+      @req = Rack::Request.new(env)
+      path = @req.path_info
+      request_method = @req.request_method.downcase.to_sym
+      return [500, {}, []] if path == "/favicon.ico"
+      controller, action = get_controller_and_action_for(path)
+      response = controller.new.send(action)
+      [200, {"Content-Type" => "text/html"}, [response]]
+    end
+
+    def get_controller_and_action_for(path, verb)
+      _, controller, action, others = path.split("/", 4)
+      require "#{controller.downcase}_controller.rb"
+      controller = Object.const_get(controller.capitalize! + "Controller")
+      [controller, "#{verb}_#{action}"]
+    end
+  end
 end
