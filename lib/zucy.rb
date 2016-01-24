05f33bcfd823ddc1753df20c3c65caf01b9524a2
Ikem Okonkwo <ikem.okonkwo@andela.com>
2016-01-24 23:43:17 +0100

    create route class

--- lib/zucy.rb
+++ lib/zucy.rb
@@ -1,6 +1,8 @@
 require "zucy/version"
 require "zucy/dependencies"
 require "zucy/utility"
+require "zucy/routing/mapper"
+require "zucy/routing/route"
 require "zucy/routing/router"
 
 require "zucy/application"
